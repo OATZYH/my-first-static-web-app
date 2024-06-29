998fe22e9ab5964ae82961c3ba75961e5a863d69
Sarun Khumthai <srnkhumthai@gmail.com>
2024-06-29 15:21:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = "World";
+
+   const alert = () => {
+    alert("Hello");
+  }
+  return (
+    <>
+      <div>Hello {value}</div>;
+      <button onClick={alert}>Click me</button>
+    </>
+  );
 }
 
 export default App;
